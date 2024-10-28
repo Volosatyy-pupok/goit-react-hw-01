fabbe47965e563d820a02d4546616fdc4f35b34b
Volosatyy-pupok <derevyanko.2001.oleg@gmail.com>
2024-10-28 18:12:18 +0200

    update

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,29 +1,42 @@
-import s from "./TransactionHistory.module.css"; // Імпортуємо файли стилів
+import s from "./TransactionHistory.module.css";
 
-const TransactionHistory = ({ items }) => {
+import PropTypes from "prop-types";
+
+export const TransactionHistory = ({ items }) => {
   return (
     <table className={s.transactionHistory}>
-      {" "}
-      {/* Додавання класу з файлу стилів */}
       <thead>
         <tr>
-          <th className={s.tableHead}>Type</th>{" "}
-          {/* Додавання класу для заголовку */}
-          <th className={s.tableHead}>Amount</th>
-          <th className={s.tableHead}>Currency</th>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
         </tr>
       </thead>
+
       <tbody>
-        {items.map((item) => (
-          <tr key={item.id}>
-            <td>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
-          </tr>
-        ))}
+        {items.map((el) => {
+          return (
+            <tr key={el.id}>
+              <td>{el.type}</td>
+              <td>{el.amount}</td>
+              <td>{el.currency}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default TransactionHistory;
